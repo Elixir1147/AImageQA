f735d290b85a5a2994b87393a89923160f6ef3d5
elixir <solaris3029@gmail.com>
2023-12-04 07:10:06 +0900

    form is moved center

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,26 @@
-import {NextPage} from 'next';
+import {NextPage,Metadata} from 'next';
+
+export const metadata:Metadata = {
+  title:'Login'
+}
 
 const LoginPage : NextPage = () => {
-  return(
+  return (
     <main>
-      <label htmlFor="email">メールアドレス</label>
-      <input name="email" id="email" type="email"></input>
-      <label htmlFor="password">パスワード</label>
-      <input name="password" id="password" type="password"></input>
-      <input type="submit" value="ログイン"/>
+      <div className="flex h-screen justify-center items-center">
+        <div className="flex flex-col">
+          <a className="text-xl">ログイン</a>
+          <div className="flex my-2 flex-col">
+            <label htmlFor="email-address">メールアドレス</label>
+            <input type="email" id="email-address" name="email-address" required></input>
+          </div>
+          <div className="flex my-2 flex-col">
+            <label htmlFor="password">パスワード</label>
+            <input type="password" id="password" name="password" required></input>
+          </div>
+          <input type="submit" value="登録" className="my-3 bg-cyan-400 hover:bg-cyan-500 active:bg-cyan-600 text-center text-white"/>
+        </div>
+      </div>
     </main>
   );
 }
