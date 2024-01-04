3de8169f6d03b38fd0739c54c120d5bcdae8e6eb
root <root@MyComputer>
2024-01-04 12:41:12 +0900

    make fundamental signup page.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,33 +1,66 @@
-import {NextPage} from 'next';
+'use client'
 import { Metadata } from 'next';
+import { registerUser } from '@/lib/registerUser';
+import { useFormState } from 'react-dom'
 
 export const metadata:Metadata = {
   title: 'Sign Up',
 }
 
-const SignUpPage : NextPage = () => {
+export default function SignUpPage(props:{onClickFunction:()=>Promise<void>}): JSX.Element{
   return (
     <main>
       <div className="flex h-screen justify-center items-center">
-        <div className="flex flex-col">
+        <form className="flex flex-col" action={async(formData)=>{
+          const message= await registerUser(formData);
+          if(typeof message !== 'undefined'){
+            window.alert(message.message)
+            console.debug(message.message)
+          }
+        }}>
           <a className="text-xl">会員登録</a>
           <div className="flex my-2 flex-col">
             <label htmlFor="user-name">ユーザー名</label>
-            <input type="email" id="email-address" name="email-address" required></input>
+            <input type="text" id="user-name" name="user-name" required></input>
           </div>
           <div className="flex my-2 flex-col">
-            <label htmlFor="email-address">メールアドレス</label>
-            <input type="email" id="email-address" name="email-address" required></input>
+            <label htmlFor="mail-address">メールアドレス</label>
+            <input type="email" id="mail-address" name="mail-address" required></input>
           </div>
           <div className="flex my-2 flex-col">
             <label htmlFor="password">パスワード</label>
             <input type="password" id="password" name="password" required></input>
           </div>
-          <input type="submit" value="登録" className="my-3 bg-cyan-400 hover:bg-cyan-500 active:bg-cyan-600 text-center text-white"/>
-        </div>
+          <input type="submit" className="my-3 bg-cyan-400 hover:bg-cyan-500 active:bg-cyan-600 text-center text-white"/>
+        </form>
       </div>
     </main>
   );
 }
 
-export default SignUpPage;
+// export function getStaticProps():{props:{onClickFunction:()=>Promise<void>}}{
+//   const onClickFunction = async()=>{
+//     console.debug("onclick is called")
+//     const userName=document.getElementById("user-name")
+//     const mailAddress=document.getElementById("email-address")
+//     const password=document.getElementById("password")
+//     if(userName!==null && mailAddress!==null && password!==null){
+//       await fetch(
+//         "http://localhost:3000/users",{
+//           method:"POST",
+//           body:JSON.stringify({
+//             userName: (userName as HTMLInputElement).value,
+//             mailAddress: (mailAddress as HTMLInputElement).value,
+//             password: (password as HTMLInputElement).value,
+//           })
+//         }
+//       )
+//     }
+//   }
+  
+//   return {
+//     props: {
+//       onClickFunction,
+//     }
+//   }
+// }
